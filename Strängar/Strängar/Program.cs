bc0419bfe18f0420bd2335ec25fe05bf9e5d1589
jakobEk <ek.jacke@gmail.com>
2017-03-10 11:59:57 +0100

    Add files via upload

---
+++ Strängar/Strängar/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strängar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Övning 1
+            Console.WriteLine("\u0056\u00E4\u0157\u006D\u0064\u00F6\u0020\u0047\u0079\u006D\u006E\u0061\u0073\u0069\u0075\u006D");
+            */
+            /* Övning 2
+            Console.WriteLine("\"Hello World\"");
+            */
+            /* Övning 3a
+            string str = "Hello World";
+            string ver = str.ToUpper();
+            Console.WriteLine(ver);
+            */
+            /* Övning 3b
+            string str = "Hello World";
+            string gem = str.ToLower();
+            Console.WriteLine(gem);
+            */
+            /* Övning 4
+            Console.WriteLine("Hello World");
+            string hello = "Hello World";
+            Console.WriteLine(hello + " innehåller " + hello.Length + " tecken lång");
+            */
+            /* Övning 5
+            Console.Write("Mata in text: ");
+            string inmatat = Console.ReadLine();
+            char[] inmatatArray = inmatat.ToCharArray();
+
+            for (int i = inmatat.Length - 1 ; i >= 0; i--)
+            {
+                Console.Write(inmatat[i]);
+            }
+            */
+
+            string textMedParam = "{0} elefant{1} balanserade på en liten liten spindeltråååd.\n"
+                                  + "Det tyckte {2} var så intressant,\n"
+                                  + "så {2} gick och hämtade en annan elefant.\n";
+
+            string finalText = string.Format(textMedParam, "En", "", "de");
+            Console.WriteLine(finalText);
+            
+            for (int i = 1; i < 10; i++)
+            {
+                finalText = string.Format(textMedParam, ++i, "er", "de");
+                Console.WriteLine(finalText);
+                if (i == 10)
+                {
+                    Console.WriteLine
+                }
+            } 
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WhileSatsen/WhileSatsen/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileSatsen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Övning 1.1
+            int i = 0;
+            while (i++ < 100)
+            {
+                Console.WriteLine(i);
+            }
+            */
+            /* Övning 1.2
+            Console.Write("Lösenord: ");
+            string losen = Console.ReadLine();
+
+            while (losen != "Feelthebern")
+            {
+                Console.WriteLine("Fel lösenord!");
+                losen = Console.ReadLine();
+            }
+            Console.WriteLine("Välkomen!");
+            */
+            /* Övning 2.1
+            int i = 1;
+     
+            while (i <= 100)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            */
+            /* Övning 2.2
+            int i = 10;
+            while (i >= 1)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+            */
+            /* Övning 2.3
+            Console.Write("SKriv valfri text: ");
+            string text = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine(text);
+            }
+            */
+            /* Övning 2.4
+            Console.Write("Gissa ett tal mellan 1 och 100: ");
+            string inmatat = Console.ReadLine();
+            int tal = int.Parse(inmatat);
+            int n = 0;
+
+            while (tal != 91)
+            {
+                if (tal < 91)
+                {
+                    Console.WriteLine("För lågt!");
+                    inmatat = Console.ReadLine();
+                    tal = int.Parse(inmatat);
+                    n++;
+                }
+                else if (tal > 91)
+                {
+                    Console.WriteLine("För högt!");
+                    inmatat = Console.ReadLine();
+                    tal = int.Parse(inmatat);
+                    n++;
+                }
+            }
+            Console.WriteLine("Grattis det tog dig " + n + " gissningar!");
+            */
+            /* Övning 2.5
+            int fib_0 = 0;
+            int fib_1 = 1;
+            int fib_n = 1;
+
+            while (fib_n < 1000000)
+            {
+                Console.WriteLine(fib_n + " ");
+                fib_n = fib_0 + fib_1;
+                fib_0 = fib_1;
+                fib_1 = fib_n;
+            }
+            */
+
+            Console.Write("Ange saldo på konto (kr): ");
+            string inmatat_1 = Console.ReadLine();
+            int saldo = int.Parse(inmatat_1);
+
+            Console.Write("Ange ränta (%): ");
+            string inmatat_2 = Console.ReadLine();
+            int ranat = int.Parse(inmatat_2);
+
+            Console.Write("Ange mål (kr): ");
+            string inmatat_3 = Console.ReadLine();
+            int mål = int.Parse(inmatat_3);
+
+            while (true)
+            {
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
