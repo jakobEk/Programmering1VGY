2c0039f7c219bd7b48a525428e87bd4134822e6a
jakobEk <ek.jacke@gmail.com>
2017-03-10 11:51:14 +0100

    Add files via upload

---
+++ Felhantering/Felhantering/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Felhantering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inkomst = 0 , timmar = 0;
+                try
+                {
+                    Console.Write("Ange din inkomst: ");
+                    inkomst = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                }
+                try
+                {
+                    Console.Write("Ange antal timmar: ");
+                    timmar = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                }
+            Console.WriteLine("Din timlön blev: " + (inkomst / timmar) + " kr/h");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Form2/Form2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form2
+{
+    public partial class Form1 : Form
+    {
+        DateTime start;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Start")
+            {
+                start = DateTime.Now;
+                timer1.Start();
+                button1.Text = "Stop";
+            }
+            else
+            {
+                timer1.Stop();
+                button1.Text = "Start";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan tid = DateTime.Now - start;
+            label1.Text = tid.Minutes.ToString().PadLeft(2, '0')
+            + ":" + tid.Seconds.ToString().PadLeft(2, '0')
+            + ":" + tid.Milliseconds.ToString().PadLeft(3, '0');
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Select();
+        }
+    }
+}
